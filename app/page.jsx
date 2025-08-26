97de611bcfb8a7d07f75fc30aef8b809002b3ba2
Amune Sireyami <praiseamune15@gmail.com>
2025-08-26 13:44:05 +0100

    changes

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      Hello World!
+      <h1 className="bg-red-500 text-white p-5 text-2xl font-bold text-center">
+        THIS IS THE MAIN BRANCH
+      </h1>
     </div>
   );
 }
